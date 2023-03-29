9048dedb45fbef73cb6e4e5ceaf3d8df47b91086
twoics <second_vozhzhov@mail.ru>
2023-03-29 21:50:14 +0700

    Import fix

--- app/src/main/java/com/twoics/geo/screens/Details.kt
+++ app/src/main/java/com/twoics/geo/screens/Details.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScaleg
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
